ea9fa16d32b7e936090991cc0294385c0468b923
Daniel <emil.encalada@udla.edu.ec>
2025-07-08 07:12:57 -0500

    Agregar archivos de proyecto.

---
+++ ExamenProgreso3EncaladaD/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenProgreso3EncaladaD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
